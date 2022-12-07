c2109f4348d131cba0beb8ef60806cad6fa65a10
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-12-07 17:39:31 +0100

    Update CredentialsValidation.js

--- src/CredentialsValidation.js
+++ src/CredentialsValidation.js
@@ -2,14 +2,20 @@ import UserDatabase from "./userdb.js";
 
 async function CredentialsValidation(username,password){
     const username_exists = await UserDatabase.raw(`select * from user where username='${username}'`);
-    if(5<username.length<12 && username_exists.length == 0){
-        if(5<password.length<12 && password.match(/^(?=.[!?.:])(?=.*\d)/)){
-            return true;
-        }
-        else{
-            throw new Error ('Password must be between 5-12 characters and contain at least one digit and at least one symbol !?.:');
-        }
+    console.log(username_exists);
+    const username_validation = username.length > 5 && username.length < 12 && username_exists.length == 0;
+    console.log("the username: ", username_validation);
+    const password_validation = password.length > 5 || password.length < 12 && password.match(/^(?=.[!?.:])(?=.*\d)/);
+    console.log("the password: ", password_validation);
+    if(!username_validation){
+        throw new Error("The username is not valid!");
     }
-    return false;  
+    else if(!password_validation) {
+        throw new Error("The password is not valid!");
+    }
+    else{
+        return true;
+    }
 }
+
 export default CredentialsValidation;
