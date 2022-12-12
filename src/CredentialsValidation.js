25de09551fa29cadcbade669a6803957a40150f3
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-12-12 13:30:45 +0100

    Update CredentialsValidation.js

--- src/CredentialsValidation.js
+++ src/CredentialsValidation.js
@@ -4,7 +4,7 @@ async function CredentialsValidation(username,password){
     const username_exists = await database.raw(`select * from users where username='${username}'`);
     const reg_expression = /(?=.*[!?.:])(?=.*\d)/;
     const username_validation = username.length > 5 && username.length < 12 && username_exists.length == 0;
-    const password_validation = password.length > 5 || password.length < 12 && password.match(reg_expression);
+    const password_validation = password.length > 5 || password.length < 12 && reg_expression.test(password);
     
     if(!username_validation){
         throw new Error("The username is not valid!");
